31f2368e8cf1fc6a8e6f6a7509c1ad0e6ff0c3c8
Inthrayuth Mahaphol and Nick Lyman <nick@>
2016-05-25 10:38:46 -0700

    Use API key to display humidity for any city a user enters.

--- js/template.js
+++
@@ -1,7 +0,0 @@
-exports.Template = function(test){
-  this.test = test;
-}
-
-exports.Template.prototype.print = function() {
-  return "Hello " + this.test;
-}
---
+++ js/weather.js
@@ -0,0 +1,7 @@
+// exports.Weather = function(test){
+//   this.test = test;
+// };
+//
+// exports.Weather.prototype.print = function() {
+//   return "Hello " + this.test;
+// };
